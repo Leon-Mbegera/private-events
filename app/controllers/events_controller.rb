9b369319cf825d11eb140ca6d18742bd97f5166e
Leon-Mbegera <mbegeraleon89@gmail.com>
2021-06-25 12:19:33 +0300

    Create events params

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -6,5 +6,16 @@ class EventsController < ApplicationController
   
   def create
     @event = current_user.events.build(event_params)
+
+    if @event.save
+      redirect_to @event
+    else
+      render :new
+    end
   end
+
+  private
+    def event_params
+      params.require(:article).permit(:event_name, :event_location, :event_date)
+    end
 end

a8f675ef3f15fc665fa99d07b6bdf95f62a35724
Leon-Mbegera <mbegeraleon89@gmail.com>
2021-06-28 15:50:59 +0300

    List events previously attended by user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,6 @@ class UsersController < ApplicationController
   def show
     @events = current_user.events.all
     @created_events = current_user.events
+    @past_events = current_user.attended_events.previous
   end
 end

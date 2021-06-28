84e47aa5e0f7593ff662c2acb3540669496029f8
Leon-Mbegera <mbegeraleon89@gmail.com>
2021-06-28 15:40:10 +0300

    List upcoming events in the index action of Event controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,6 +2,7 @@ class EventsController < ApplicationController
   def index
     @events = Event.all
     @past_events = Event.previous_events
+    @upcoming_events = Event.upcoming_events
 
   end
 

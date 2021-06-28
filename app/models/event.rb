ec1a804dde1cdd86c493192ed9f2f4d9c8fd518a
Leon-Mbegera <mbegeraleon89@gmail.com>
2021-06-28 18:44:37 +0300

    Create scope for past events

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,8 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User'
   has_many :turnouts
   has_many :attendees, class_name: 'User', through: :turnouts, source: 'attendee'
+
+  scope :past_events, Proc.new {where("date < ?", DateTime.now)}
+  
+
 end
